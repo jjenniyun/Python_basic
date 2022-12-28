69a29dfaf181ef9a1bd4eed09aeb18a580f79fe1
jjenniyun <hjkjj1847@gmail.com>
2022-12-29 00:53:53 +0900

    python_web(22.12.29)

--- Web_Scrapping_basic/6_bs4.py
+++ Web_Scrapping_basic/6_bs4.py
@@ -16,5 +16,23 @@ soup = BeautifulSoup(res.text, "lxml")
 #print(soup.find(attrs={"class":"Nbtn_upload"})) # class":"Nbtn_upload" 인 어떤 요소를 찾아줘
 
 #print(soup.find("li",attrs={"class":"rank01"}))
-rank1 = soup.find("li",attrs={"class":"rank01"})
-print(rank1.a)
+#rank1 = soup.find("li",attrs={"class":"rank01"})
+#print(rank1.a.get_text())
+#print(rank1.next_sibling)
+# rank2 = rank1.next_sibling.next_sibling
+# rank3 = rank2.next_sibling.next_sibling
+# print(rank3.a.get_text())
+# rank2 = rank3.previous_sibling.previous_sibling
+# print(rank2.a.get_text())
+#print(rank1.parent)
+# rank2 = rank1.find_next_sibling("li")
+# print(rank2.a.get_text())
+# rank3 = rank2.find_next_sibling("li")
+# print(rank3.a.get_text())
+# rank2 = rank3.find_previous_sibling("li")
+# print(rank2.a.get_text())
+
+#print(rank1.find_next_siblings("li")) # "li"해당하는 내용 siblings가져오기
+
+webtoon = soup.find("a", text="연애혁명-421. 원더랜드 (2)")
+print(webtoon)
