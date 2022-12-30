f2d44eb3076c461a5d414b7e580e18c40b211d81
jjenniyun <hjkjj1847@gmail.com>
2022-12-31 00:52:11 +0900

    python_flights수정(22.12.31)

--- Web_Scrapping_basic/14_selenium_flight.py
+++ Web_Scrapping_basic/14_selenium_flight.py
@@ -1,64 +1,68 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-browser = webdriver.Chrome('chromedriver.exe')  # 크롬 드라이버가 위치한 경로 대입 필요
+
+def wait_util(xpath_str):
+    WebDriverWait(browser, 30).until(
+        EC.presence_of_element_located((By.XPATH, xpath_str)))
+
+
+browser = webdriver.Chrome()  # 크롬 드라이버가 위치한 경로 대입 필요
 browser.maximize_window()  # 창 최대화
 
-# browser = webdriver.Chrome()
-# def set_chrome_driver():
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-# return driver
-
 # 네이버 항공권
-url = "https://www.naver.com"
+url = "https://flight.naver.com/"
 # browser = set_chrome_driver()
 
 browser.get(url)  # url로 이동
-elem = browser.find_element(By.NAME, "query")
-elem.click()
-elem.send_keys("네이버 항공")
-elem.send_keys(Keys.ENTER)
-# elem = browser.find_element(By.CLASS_NAME, "link_name")
-# elem.click()
+# 팝업창 닫기
+main = browser.window_handles
+# 팝업이 있는지 체크
+for i in main:
+    if i != main[0]:
+        browser.switch_to.window(i)
+        browser.close()
+browser.switch_to.window(main[0])
 
-# https://www.youtube.com/watch?v=qhy8I4ChCuw(find_element 셀레니움4버전 바뀌면서 사용하는 방법)
 # 가는 날 선택 클릭
-browser.find_element(
-    By.CLASS_NAME, "sp_flight flight_btn flight_btn_calendar").click()
+begin_date = browser.find_element(By.XPATH, '//button[text()= "가는 날"]')
+begin_date.click()
 
-# browser.find_element(By.CLASS_NAME, "sc-furvIG lkLLxz num").click() # 이번달 31일 선택
+# time.sleep(1) # 1초 대기
+wait_util('//b[text()="31"]')  # 나올때까지 30초 대기
 
-# 이번달 31일, 다음달 2일 선택
-# browser.find_elements(By.LINK_TEXT, "31")[0].click() # 이번달
-# browser.find_elements(By.LINK_TEXT, "11")[1].click() # 다음달
+# 이번달 31일 선택
+day31 = browser.find_elements(By.XPATH, '//b[text()="31"]')
+# len(day31)
+day31[0].click()
 
-# 다음달 27, 28일 선택
-# browser.find_elements(By.LINK_TEXT, "27")[1].click()
-# browser.find_elements(By.LINK_TEXT, "28")[1].click()
+# 다음달 27일 선택
+wait_util('//b[text()="27"]')
+day27 = browser.find_elements(By.XPATH, '//b[text()="27"]')
+day27[1].click()
 
-# 도쿄 선택
-# browser.find_element(By.XPATH, "//*[@id='__next']/div/div[1]/div[6]/div/div[2]/div/ul/li[1]/button/figure/img").click()
-browser.find_element(By.CLASS_NAME, "_name").click()
-browser.find_element(
-    By.XPATH, "//*[@id='_flight_section']/div/div[2]/div[1]/div[2]/div[1]/div[2]/div/div/table[3]/tbody/tr[1]/td[2]/a").click()
+# 도착지 선택
+arrival = browser.find_element(By.XPATH, '//b[text()="도착"]')
+arrival.click()
+
+# 국내 선택
+domestic = browser.find_element(By.XPATH, '//button[text() = "국내"]')
+domestic.click()
+
+# 제주 선택
+jeju = browser.find_element(By.XPATH, '//i[contains(text(), "제주국제공항")]')
+jeju.click()
 
 # 항공권 검색 클릭
-browser.find_element(By.CLASS_NAME, "sp_flight flight_btn_search").click()
+search = browser.find_element(By.XPATH, '//span[contains(text(), "항공권 검색")]')
+search.click()
 
-# 로딩 처리
-try:
-    elem = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located(
-        (By.XPATH, "//*[@id='__next']/div/div[1]/div[6]/div/div[3]/div[1]/div/button")))
-    # 성공했을 때 동작 처리
-    print(elem.text)  # 첫번째 결과 출력
-finally:
-    browser.quit()
+elem = WebDriverWait(browser, 30).until(EC.presence_of_element_located(
+    (By.XPATH, '//div[@class="domestic_Flight__sK0eA result"]')))
+print(elem.text)
+
+input("종료하려면 enter키를 입력하세요")
+browser.quit()
