3fdf39babb2fba65b58c340fe9239b1bb704bfbf
jjenniyun <hjkjj1847@gmail.com>
2023-01-31 23:56:19 +0900

    python_PuzzleBobble게임만들기 시작(23.01.31)

--- gui_project/6_advanced_screenshot.py
+++ gui_project/6_advanced_screenshot.py
@@ -8,5 +8,7 @@ def screenshot():
     img.save("image{}.png".format(curr_time))
 
 keyboard.add_hotkey("F9", screenshot) # 사용자가 f9키를 누르면 스크린 샷 저장
+#keyboard.add_hotkey("a", screenshot) # 사용자가 'a'키를 누르면 스크린 샷 저장
+#keyboard.add_hotkey("ctrl+shift+s", screenshot) # 사용자가 'ctrl+shift+s'키를 누르면 스크린 샷 저장
 
 keyboard.wait("esc") # 사용자가 esc를 누를 때까지 프로그램 수행
